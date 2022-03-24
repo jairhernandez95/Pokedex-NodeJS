9b9025c046855702665ec3049fc51f3e3c7b7aa7
Jair Hernández <jair_097@hotmail.com>
2022-03-24 13:43:53 -0700

    Update main.js and styles.css

--- main.js
+++ main.js
@@ -167,12 +167,17 @@ function showModalPokemon(element)
                 imageWidth: 300,
                 imageAlt: 'Custom image',
             })
-        }
-    ).catch(    
-        Swal.fire({
-            title: `Sorry :(`,
-            text: `We dont have enough information about this pokemon, we need to Ash to keep us updated 😡, if you see him please tell him.`,
-        })
-    )
+        }).catch(
+            Swal.fire({
+                title: `Loading...`,
+                showCancelButton: false,
+                showConfirmButton: false
+                // text: `We dont have enough information about this pokemon, we need to Ash to keep us updated, if you see him please tell him 😡`,
+        }))
 }
+// Swal.fire({
+//     icon: "error",
+//     title: `Error`,
+//     text: `We dont have enough information about this pokemon, we need to Ash to keep us updated, if you see him please tell him 😡`,
+// })
 getData();
